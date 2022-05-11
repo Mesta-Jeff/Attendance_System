b666c1bd0d3c6052664b0cc55e6d8434e36ef6bf
Odeneho1-cell <72224657+Odeneho1-cell@users.noreply.github.com>
2022-05-11 05:02:18 +0000

    Add files via upload

---
+++ cls_DES.cs
@@ -0,0 +1,22 @@
+﻿ 
+        public string EncryptionDES(string strText)
+        {
+            string strKey="AB14BB29";
+            byte[] bykey = { };
+            byte[] iv = { 0x01, 0x06, 0x06, 0x02, 0x09, 0x06, 0x02, 0x07 };
+            try
+            {
+                bykey = Encoding.UTF8.GetBytes(strKey);
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+                byte[] input = Encoding.UTF8.GetBytes(strText);
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(bykey, iv), CryptoStreamMode.Write);
+                cs.Write(input, 0, input.Length);
+                cs.FlushFinalBlock();
+                return Convert.ToBase64String(ms.ToArray());
+            }
+            catch (Exception ex) { }
+            return null;
+        }
+
+        
